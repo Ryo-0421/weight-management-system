a62f08a0c3a460b9531c965172f96d8342fdf212
Ryo_Utsumiya <ryo.u.forbusiness@gmail.com>
2023-08-13 14:31:38 +0900

    first commit

---
+++ src/main/java/com/example/weight_management_system/WeightManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.weight_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weight_management_system/WeightManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weight_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
